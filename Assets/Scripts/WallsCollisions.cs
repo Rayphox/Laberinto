3aab428b34de907b3e91a121a53b8f2c52109f4d
Rayphox <danirper05@gmail.com>
2024-10-02 19:25:15 +0100

    Paredes de colores
    Ahora las paredes cambian de color al tocarlas

--- Assets/Scripts/WallsCollisions.cs
+++ Assets/Scripts/WallsCollisions.cs
@@ -10,12 +10,30 @@ public class WallsCollisions : MonoBehaviour
     [Serializefield]
     Material materialParedTocada;
 
+    bool paredRoja = false;
+
+    float tiempoEnRojo = 5f;
+
+    private void Update()
+    {
+        if (paredRoja == true)
+        {
+            tiempoEnRojo = tiempoEnRojo - Time.deltaTime;
+            if (tiempoEnRojo < 0.0f)
+            {
+                gameObject.GetComponent<MeshRenderer>().material = materialPorDefecto;
+                paredRoja = false;
+                tiempoEnRojo = 5f;
+            }
+        }
+    }
+
     private void OnCollisionEnter(Collision col)
     {
         if (col.gameObject.tag == "Player")
         {
-            Debug.Log(col.gameObject.name);
             gameObject.GetComponent<MeshRenderer>().material = materialParedTocada;
+            paredRoja = true;
         }
     }
 
